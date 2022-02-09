1b8081f4a4ae3b482edf58b97b05edbffcc47c76
deperazar <danielpblog2014@gmail.com>
2022-02-09 12:16:27 -0500

    Hooks conversion

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
@@ -6,43 +6,38 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css'
 // The parent commands the state to its children STATE >> props
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = { //Something that only the parent can change // This is what changes in the app
-            robots: [],
-            searchField: ''
-        }
-    }
+function App(){
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users})); 
+        .then(response => response.json())
+        .then(users => setRobots(users))
+    }, [count]); // only run if count changes
+    
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)       
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value})        
-    }
-
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        });
-        return !robots.length? <h1 className="tc">Loading</h1> :
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    });
+    console.log(count);
+    return !robots.length? <h1 className="tc">Loading</h1> :
         (
             <div className="tc">
                 <h1 className="ma10">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <button onClick={() => setCount(count + 1)}>Click me</button>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={filteredRobots} />
                     </ErrorBoundary>
                 </Scroll>                    
             </div>
-        );            
-    }
+        );
 }
 
 export default App;
